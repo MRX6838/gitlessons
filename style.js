9dc03ad2886b266d313e596309da56af96e0decf
MRX6838 <gorgevorgyan255581@gmail.com>
2022-11-03 16:47:12 +0400

    changes

---
+++ style.js
@@ -0,0 +1,936 @@
+// debugger
+
+// 51
+// let n = prompt();
+// let miavor = +n[2]
+// let tasnavor = +n[1];
+// let haruravor = +n[0];
+
+// if ( miavor > tasnavor && miavor > haruravor) {
+//     console.log( miavor)
+// }
+// else if(
+//     tasnavor > miavor && tasnavor> haruravor ){
+//         console.log(tasnavor);
+
+//     }else if (haruravor > tasnavor && haruravor > miavor){
+//         console.log(haruravor);}
+
+
+
+
+
+// let n = prompt();
+// let miavor = +n[2];
+// let tasnavor = +n[1];
+// let haruravor = +n[0];
+
+// if (miavor == tasnavor || miavor == haruravor || tasnavor == haruravor ) {
+//     console.log( true)
+// }
+// else{
+//     console.log(false);
+
+// }
+
+
+// let n = prompt();
+// let miavor = +n[2];
+// let tasnavor = +n[1];
+// let haruravor = +n[0];
+
+// if( n > 300 ){
+
+//  console.log(tasnavor % miavor);
+
+// }   
+
+// else if(n < 300 ){
+//   console.log(haruravor % miavor);
+
+// }
+
+// let n = prompt();                         
+// let miavor = +n[2];
+// let tasnavor = +n[1];
+// let haruravor = +n[0];
+//  if(tasnavor + haruravor < 5 ){
+//      console.log(true);
+//  } 
+
+// else{
+//     console.log(false);
+
+// }
+
+// let n = prompt();                         
+// let miavor = +n[2];                    gggg
+// let tasnavor = +n[1];
+// let haruravor = +n[0];
+
+// if(miavor > tasnavor && miavor > haruravor && tasnavor >haruravor ){
+//     console.log(miavor, tasnavor, haruravor);{
+
+
+
+// for (let i = 1; i  <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+
+//     }
+//     console.log(i);
+
+// }
+
+// let b =[5,6,10,46]
+// console.log(b);
+
+// let a = [7, 3, 7, 1, 4, 6, 'barev', 4, 15, 357,]
+// a[0] = 5
+// a[1] = 'text'
+// a[6] = 16
+// console.log(a);
+
+
+// let y = [4,[6,8,10],66,83,9,[2,1,9,[6,7,8,]]]
+// let a = y[5][3][2]
+// console.log(a);
+
+
+// function Gago() {
+//     const Anuner = ['Gago', 'Virab', "Sipan", "Vemir",'stipan']
+//     for (let i = 0; i <= Anuner.length; i++) {
+//         if (Anuner[i] === 'Gago') {
+//             console.log('chist')
+//         } else if (Anuner[i] === 'Virab') {
+//             console.log('chisht');
+//         } else {
+//             console.log('vad es ');
+//         }
+//     }
+// }
+// Gago();
+
+// function Gago() {
+//     let a = ["Gago", 'vzgo', "Aram"]
+//     a.filter((anun) => {
+//         {anun == "Aram" ? console.log(true) : console.log(false);}
+//     })
+//  }
+// Gago();
+
+
+
+// const Artash = () => {
+//     const ARR = [1, 2, 3, 4, 5, { anun: 'Gago' }, { tariq: 18 }, [null], undefined, NaN, 'sapo', 'gags'];
+
+//     ARR.filter((tiv) => {
+//         if (typeof tiv === "number") {
+//             console.log(tiv);
+
+//         }
+//     })
+// }
+// Artash();
+
+
+// const tver = [5, 3, 7, 41];
+// let x = tver.sort();
+// console.log(x);
+
+// const arr = ["Alfa", "Gago", "levon"];
+// let x = arr.filter((tiv) =>{
+//         if (tiv === "Alfa") {
+//             console.log(true)
+
+//         }else {
+//             console.log(false)
+
+//         }
+//     })
+
+
+// const arr = [11,10,33,90,65];
+// let x = arr.every((myNumbers) =>{
+//     return myNumbers >=11
+// })
+// console.log(x);
+
+
+
+
+
+// const arr =[4,9,2,3,45,8,7,9,1]
+// let x = arr.some((myNumbers) => {
+//     return myNumbers >= 10
+// })
+//  console.log(x);
+
+
+
+// var arr = [2, 8, 9, 12, 4];
+// var myMap = arr.map((i) => {
+//     return (i > 3)
+// })
+// console.log(myMap);
+
+
+// var arr =["bnjb", 45,"adncc"];
+// var anun = prompt("gri anun axganun");
+// var tariq = prompt("gri tariqt");
+// var x = arr.push(anun,tariq);
+// console.log(arr);
+
+
+// var arr =[5,9,8];
+// var x =arr.pop();
+// console.log(arr);
+
+
+// var arr = [5,8];
+// var x =arr.unshift(10,69,50)
+// console.log(arr);
+
+// var arr =[5,8,10,15]
+// var x =arr.shift()
+// console.log(arr);
+
+
+// var arr =[5,4,0,9,88,65,46]
+// let x = arr.slice(2,5)
+// console.log(x);
+
+
+// var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// var x= arr.splice(2,7,'gago','gago');
+// console.log(arr);
+
+
+// var a =[10,7,99];
+// var b =[19,90];
+// var c =a.concat(b);
+// console.log(c);
+
+// var arr = [2, 98, 8, 88, 0];
+// var b = arr.join("");
+// console.log(b);
+
+
+
+
+
+
+
+//  das 1
+// let y =[]
+// let t =[]
+// var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var myMap = a.map((i) => {
+//     if (i % 2 == 0 ) {
+//         y.push(i)
+//     } else t.push(i)
+// })
+// console.log(y);
+// console.log(t);
+
+
+
+
+
+// das2
+// let a = [5, 10, 40]
+
+
+// let b = [4, 10, 5]
+
+
+// if (a[2] == b[1] * 4) {
+//     console.log(true);
+
+// } else{
+//     console.log(false);
+
+// }
+
+
+// var a = [5, 8, 10, 15, 40, 90]
+// var b = []
+// a.filter((x) => {
+//     if(x > 30 ){
+//         b.push(x)
+//         console.log(x);
+//     }
+
+// })
+
+
+// var a = [5, 1, 4, 7, 3, 8]
+// var b = []
+//  let x = a.sort();
+//     console.log(x);
+
+//  debugger
+
+// let anun = prompt()
+// let tarik = prompt()
+// let d = []
+//     if (typeof anun === 'string'&& typeof tarik === 'number'){
+//         d.push(anun, tarik)
+//     }else{
+//         console.log(false);
+//     }
+//     console.log(d);
+
+
+// let a = ['esh gzox', undefined, 10, 7,]
+// let x = a.shift()
+//  a.push(x)
+// console.log(a);
+
+
+// let a = [1, 7, 9, 4, 7, 3, 5, 6, 1, 2,]
+// let b = [8, 4, 3, 9, 5, 2, 6, 4, 1, 7,]
+// let x = a.sort()
+// let c = b.sort()
+// a.pop()
+// b.pop()
+// if (a.pop > b.pop) {
+//     console.log(true);
+// }
+// else if (
+//     b.pop > a.pop
+// ) {
+//     console.log (false);
+
+// }
+
+
+
+
+// let a =[]
+// a.push(1,2,3,4,5)
+// console.log(a);
+
+
+
+
+// const axjik = {
+//     tarik: "22",
+//     tesk: "shat sirun",
+//     boy: "175 sm",
+//     achker: "kapuyt",
+//     mazer: "sev guyn",
+//     chashEpel() {
+//         const utelik = prompt("inch utelik es naxyntrum");
+//         console.log(utelik)
+//     },
+//     jurxmel() {
+//         const liter = prompt("kani litr jur es xmum");
+//         console.log("orva mej petka xmes " + liter   + " litric avel jur");
+
+//     }
+
+// }
+// axjik.chashEpel();
+// axjik.jurxmel()
+
+
+
+// const lavObekt = {
+//     firma:'Acer',
+//     model:"Swift 3",
+//     processor:"Core I7",
+//     Ram:"8gb",
+//     ekran:"13.9",
+//     mianal(){
+//         const anun = prompt("say smth");
+//         const zangvac =[1,2,3,4,5,6,7,8,9,10]
+//         if(anun === 'Armen'){
+//             zangvac.filter(axper => axper % 2 !== 0).map(armen => {
+//                 console.log(armen);
+
+//             } )
+
+//         }else{
+//             console.log(false);
+
+//         }
+//     },
+// }
+// lavObekt.mianal();
+
+// const gorc = {
+//     hastik:"cragravprox",
+//     grafik:"amenorya",
+//     gumarayin:false,
+//     showdetals(){
+//         const terneriOperator = this.gumarayin ?'gumar@ shata' :  'gumar@ kicha';
+//         console.log(`es ${this.hastik}i gorcy ${this.grafik} a ev ${terneriOperator}.`);
+
+//     }
+// };
+// gorc.showdetals()
+
+
+
+// const ashxatoxner = {
+//     tnoren:"Armen",
+//     senor_mern_stack:"Gurgen",
+//     Middle_Mern_stack:"Hayko",
+//     dasntacavan:"Paruyr"
+// };
+
+// const vladimir = Object.value(ashxatoxner);
+// console.log(vladimir);
+
+
+// const ashxatoxner = {
+//     tnoren:"Armen",
+//     senor_mern_stack:"Gurgen",
+//     Middle_Mern_stack:"Hayko",
+//     dasntacavan:"Paruyr"
+// };
+
+// const vladimir = Object.keys(ashxatoxner);
+// console.log(vladimir);
+
+
+
+// const ashxatoxner ={
+//     tnoren:'Armen',
+//     Senior_Mern_Stack:'Gurgen',
+//     Middle_Mern_stack:"Hayko",
+//     dasntacavan:"Paruyr"
+// };
+// Object.keys(ashxatoxner).forEach(amenAndam => {
+//     amenAndam ==='Senior_Mern_Stack'? console.log('k') : null;
+
+// });
+
+
+// const ashxatoxner ={
+//     tnoren: 'Armen',
+//     Senior_Mern_Stack:'gurgen',
+//     Middle_Mern_stack:'Hayko',
+//     dasntacavan:'Paruyr'
+// }
+
+// const length =Object.keys(ashxatoxner).length;
+// console.log(length);
+
+// const dasaran = {
+//     arajinAshakert:"Hayko",
+//     erkrordAshakert:"Aram",
+//     errordAshaket:"Davit",
+//     chorordAshaker:"suren",
+// };
+// const arjekner = Object.values(dasaran);
+// console.log(arjekner);
+
+
+
+// const anunAzganun ={
+//     anun:'Hayko',
+//     azganun:'kirakosyan',
+// };
+
+// const tvyalner ={
+//     ashxatank:"cragravorox",
+//     hastik:"Mirn Stack Developer"
+// };
+// const kpcrac = Object.assign(anunAzganun,tvyalner);
+// console.log(kpcrac);
+
+// let a = [
+//     {
+//         anun: 'Gor',
+//         tarik: '15',
+
+//     },
+
+//     {
+//         anun: 'Hayk',
+//         tarik: '30',
+
+//     },
+//     {
+//         anun: 'Aram',
+//         tarik: '21',
+//     },
+
+
+
+// ]
+
+// Object.values(a).forEach(meca => {
+//     meca.tarik > 24 ? console.log(meca.tarik) : false;
+// })
+
+// let a = [
+//     {
+//         anun: 'Gor',
+//         tarik: '15',
+//         masnaget: 'a'
+//     },
+
+//     {
+//         anun: 'Hayk',
+//         tarik: '33',
+//         masnaget: 'v'
+
+//     },
+//     {
+//         anun: 'Aram',
+//         tarik: '21',
+//         masnaget: 'm'
+//     },
+
+
+// ]
+// let g = prompt()
+// let b = a.forEach(arajin => {
+//     g === arajin.masnaget ? console.log(arajin.tarik) : console.log(false)
+
+// })
+
+
+
+// let a =[
+
+//     {
+//         ID:'1'
+//     },
+//     {
+//         ID:'2'
+//     },
+//     {
+//         ID:'3'
+//     }
+// ]
+// let v = prompt()
+
+// let b =a.forEach(di =>
+// {
+//     v === di.ID ?console.log(di.ID): console.log(false);
+
+// })
+
+
+// let a = [
+//     {
+//         anun: 'Gor',
+//         azganun:'Gevorgyan',
+//         tarik:'20',
+//         masnaget:'cragravorox'
+
+//     },
+//     {
+
+//         anun: 'Hayk',
+//         azganun:'Patyan',
+//         tarik:'23',
+//         masnaget:'cragravorox' 
+//     },
+//     {
+
+//         anun: 'Ando',
+//         azganun:'Mianasyan',
+//         tarik:'30',
+//         masnaget:' menejer'
+//     },
+//     {
+
+//         anun: 'Babken',
+//         azganun:'Barsexyan',
+//         tarik:'15',
+//         masnaget:'sanitar',
+//     }
+
+// ] 
+
+// let b = a.forEach(ba =>{
+//     ba.anun === 'Ando'?console.log(ba.anun,ba.azganun,ba.tarik,ba.masnaget):false;
+
+
+// })
+
+// let x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
+// let a = [];
+// let b = [];
+// for (let i = 0; i < x.length; i++) {
+//     if (x[i] % 2 == 0) {
+//         a.push(x[i])
+
+
+//     } else {
+//         b.push(x[i])
+
+
+//     }
+// }
+// console.log(b);
+// console.log(a);
+
+
+
+
+
+
+
+
+// let a = [1, 2, 34, 48, 45, 76, 7, 813, 94];
+// let p = +prompt();
+// let  y = a.map((q) => {
+//     if(p > q){
+//         y.push(q);
+
+//     }
+
+// })
+//   console.log(y);
+
+
+
+
+
+
+
+// krknutyun
+
+// let a = [1,50,47,11,98,20,25,]
+// let b =[]
+// for(let i=0; i < a.length; i++)
+// if(a [i] > 30 )
+// b.push(a[i])
+// console.log(b);
+
+
+// let a = [2, 3, 6, 7, 4, 1, 9, 8,]
+// let b = []
+// a.sort()
+// b.push(a)
+// console.log(b);
+
+
+// let a = [1,5,3,"ba",undefined]
+// let b = a.shift()
+// a.push(b)
+// console.log(a);
+
+
+// let  anunAzganun = prompt()
+// let tarik = +prompt()
+// console.log(anunAzganun,tarik);
+
+// let a = [3, 5, 2, 9, 1, 7, 4, 8, 6,]
+// let b = [, 8, 7, 6, 5, 4, 3, 2, 1,]
+// a.sort()
+// b.sort()
+// let x = b.pop()
+// let c = a.pop()
+
+// if (x > c) {
+//     console.log(x);
+
+// } else{
+//     console.log(c);
+
+// }
+//  let v = a.concat(b)
+// console.log(v);
+
+
+
+// let a = ["abgar", "esh", "alfa", "beta"]
+// let b = a.filter((miban) => {
+//     if (miban === "alfa"){
+//         console.log(true);
+
+//     }else{
+// console.log(false);
+
+//     }
+//  })
+
+
+
+// let a =[]
+// let b = [1,4,3,7,2,6,5]
+// b.sort( )
+// a.push(b)
+// console.log(a);
+
+
+
+// let a = [
+//     {
+// anun:"Gor",
+// tarik:"20"
+//     },
+//     {
+//         anun:"Hayko",
+//         tarik:"24"
+//     },
+//     {
+//         anun:"Aram",
+//         tarik:"26"
+//     }
+// ]
+
+// a.forEach(tarikner => {
+//     if (tarikner.tarik > 21) {
+//         console.log(true);
+
+//     } else {
+//         console.log(false);
+
+//     }
+
+// });
+
+
+// let a = [
+//     {
+//         anun: "Gor",
+//         tarik: "20",
+//         masnagityun: "elektrik"
+//     },
+//     {
+//         anun: "Hauyk",
+//         tarik: "24",
+//         masnagityun: "cragravorox"
+//     },
+//     {
+//         anun: "Aram",
+//         tarik: "25",
+//         masnagityun: "dasatu"
+//     },
+//     {
+//        anun:"babken",
+//        tarik:"30",
+//        masnagityun:"xadavik"
+//     }
+// ]
+// let b = prompt()
+
+// a.forEach(masnaget =>{
+//     if(b === masnaget.masnagityun ){
+//         console.log(masnaget.anun,masnaget.tarik);
+
+//     } else{
+//         console.log("chka tenc mard mer cankum");
+
+//     }
+// })
+
+
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+// let b = a.map((ma) => {
+//    if(ma % 2 == 0 ) 
+//    console.log(ma);
+
+
+
+// })
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+
+// let b = a.map((ma) => {
+//  return [ma] + '!'
+
+
+// })
+// console.log(b);
+
+// let name = prompt('what your name my friend' )
+// if (name == 'Gor' || 'gor'){
+//     console.log(prompt("welcome boss press ok"));
+//     let  username = prompt("what yor username")
+//     if(username == 'MRX6838')
+//     console.log( prompt('write password'));
+//     let password = 6838;
+//     if(password === 6838){
+//         console.log('I LOVE YOU');
+
+//     }
+
+
+// }else{
+//     console.log('fuck you');
+
+// }
+
+// let x = 1;
+// for (let x = 0; x <= 10; x++) {
+//     console.log(x);
+
+// }
+
+// let x = 10;
+// for (x = 10; x >= 1 ; x--) {
+//     console.log(x);
+
+// }
+
+// let sun = 0;
+// let i = 0;
+// for (i = 0; i <= 9; i++) {
+//     sun = sun + i;
+
+
+// }
+// console.log(sun);
+
+// let sun = 10;
+// let a = 10;
+// for (a = 10; a <= 15; a++) {
+//     sun = sun * a;
+
+// }
+// console.log(sun);
+
+
+
+
+
+// let a = 17;
+// if (a = 17) {
+//     console.log(17);
+
+// } elseif(a <=16)} 
+//     console.log(14);
+
+// {else{
+
+// }
+
+
+// setiterval jamanak
+
+// setiterval - amen eskan jamanaky mek 
+// setTimeout - eskan jamanak heto 
+// clearinterval - kangnacnum e setintervaly 
+// 1000 y 1 varkyan e 
+
+// const x = setInterval(() =>{
+//     console.log('gago');
+
+// },1000)
+
+// console.log(3);
+
+
+// setTimeout(()=>{
+//     clearInterval(x)
+// },10000);
+
+
+// setTimeout(() => {
+
+//     console.log(all)
+// }, 10000);
+
+// let anun = prompt("what your neme");
+// let azganun = prompt("what your surname");
+// let age = +prompt('how old are you ');
+// let pasword = prompt("password");
+// let all = [anun, azganun, age, pasword]
+// if(typeof age === 'number' && pasword.length>=8){
+
+// }else{
+// console.log(false);
+
+// }
+
+// setInterval(() => {
+//     console.log({ x });
+
+// }, 1000);
+// let x = 0
+// for (x = 0; x >= 100; x++)
+// {
+//     console.log(x);
+
+// }
+
+
+
+// setTimeout(() => {
+//     clearInterval(x)
+// }, 100000);
+
+// let anun = prompt()
+// let azganun = prompt()
+// let age = +prompt()
+// let password = prompt()
+
+// if (
+//     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//     ){
+// console.log(anun,azganun,age,password);
+
+//     }
+function fibonachci (num){
+    let x = [0,1];
+    for (let i = 2;i<=num;i++){
+        let a = x[i-1];
+        let b = x[i-2];
+        x.push(a+b);
+    }
+return x [num]
+
+} 
+console.log(fibonachci(6));
+
+// function fibonacci (num){
+//     let x =[0,1];
+//     for(let i = 2; i<=num; i++){
+//         let a = x[i-1];
+//         let b = x[i-2];
+//         x.push(a+b);
+//     }
+//     return x[num]
+// }
+// console.log(fibonacci(6))
+// debugger
+// function fibonachci(num) {
+//     let n1 = 0
+//     let n2 = 1
+//     let nn
+//     for (let i =  0; i<= 20; i++) {
+//         let nn = n1 + n2
+//         let n1 = n2
+//         let n2 = nn
+//     }
+//     return nn[num]
+
+// }
+// console.log(fibonachci(6));
+
+// program to generate fibonacci series up to n terms
+
+// take input from the user
+// const number = parseInt(prompt('Enter the number of terms: '));
+// let n1 = 0, n2 = 1, nextTerm;
+
+
+
+// for (let i = 1; i <= number; i++) {
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
+
+
+
+// let fibonacci = +prompt("gri mi bam ")
+// let n1 = 0;
+// let n2 = 1;
+// let n3
+// for (let i = 1; i <= fibonacci; i++) {
+//      n3 = n1 + n2;
+//     n1 = n2;
+//      n2 = n3;
+//     console.log(n3); 
+// }
+
